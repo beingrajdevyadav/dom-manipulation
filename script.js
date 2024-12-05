aa8f0879e1e3a387643ad113027d23e0838df572
Rajdev Yadav <myself.rajdev2024@gmail.com>
2024-12-06 00:19:19 +0530

    javascript functionality added.

---
+++ script.js
@@ -0,0 +1,41 @@
+// ------>>>>>------->>>>>>------>>>>>>
+//   selection of html tags
+// ------<<<<<-------<<<<<<------<<<<<<
+const createBtn = document.querySelector("#createBtn");
+const box = document.querySelector("#box");
+
+
+// ------>>>>>------->>>>>>------>>>>>>
+//   function to perform on btn click
+// ------<<<<<-------<<<<<<------<<<<<<
+createBtn.addEventListener('click', function () {
+    let tagName = document.querySelector("#tagName").value;
+    let tagText = document.querySelector("#tagText").value;
+    let tagColor = document.querySelector("#color").value;
+    let tagBGColor = document.querySelector("#bg-color").value;
+    let tagPosition = document.querySelector("#tagPosition").value;
+
+    // ------>>>>>------->>>>>>------>>>>>>
+    //  html element creation dynamically
+    // ------<<<<<-------<<<<<<------<<<<<<
+
+    let tag = document.createElement(tagName);
+    tag.innerText = tagText;
+    tag.style.color = tagColor;
+    tag.style.backgroundColor = tagBGColor;
+
+    // ------>>>>>------->>>>>>------>>>>>>
+    //   conditions to place created tag
+    // ------<<<<<-------<<<<<<------<<<<<<
+
+    if (tagPosition == "append") {
+        box.append(tag);
+    } else if (tagPosition == "prepend") {
+        box.prepend(tag);
+    } else if (tagPosition == "before") {
+        box.before(tag);
+    } else if (tagPosition == "after") {
+        box.after(tag);
+    }
+
+});
